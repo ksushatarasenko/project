a0fea61eced951e4b9bc5fe0f824f4faba1157df
Oksana <ksushatarasenko@gmail.com>
2023-12-22 11:52:21 +0100

    new

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
+import {  Routes, Route, HashRouter } from 'react-router-dom';
 import Header from './Components/header/Header'; 
 import Order from './Components/order/Order';
 import SingleProduct from './Components/products/Products/SingleProduct';
